a8cbb0cc46862d584dfad7743d4b896f63adde26
MK <masamichi.kagaya@gmail.com>
2019-09-21 19:06:07 +0900

    After add SpMenu

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,44 +3,77 @@ import { Link, NavLink } from "react-router-dom";
 import "./css/nav.styl";
 
 class Nav extends Component {
-  state = {};
+  state = {
+    spMenuActive: false
+  };
+
+  onSpMenuHandle = () => {
+    let spMenuActive = this.state.spMenuActive;
+    spMenuActive = !spMenuActive;
+    this.setState({ spMenuActive });
+  };
+
+  getSpMenuClasses() {
+    let activeCheck = this.state.spMenuActive ? "active" : "";
+    return `menu-trigger ${activeCheck}`;
+  }
+
   render() {
     return (
-      <nav className="gNav">
-        <h1>
-          <Link to="/">Autoradiograph</Link>
-        </h1>
-        <ul className="navLink">
-          <li>
-            <NavLink to="/" exact>
-              Top
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/history">History</NavLink>
-          </li>
-          <li>
-            <NavLink to="/shortmovie">Short movie</NavLink>
-          </li>
-          <li>
-            <NavLink to="/">Print</NavLink>
-          </li>
-          <li>
-            <NavLink to="/">Contact</NavLink>
-          </li>
-          <li>
-            <NavLink to="/">JP / EN</NavLink>
-          </li>
-        </ul>
-        <ul className="navCV">
-          <li>
-            <NavLink to="/">Patreon</NavLink>
-          </li>
-          <li>
-            <NavLink to="/">Log in</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <React.Fragment>
+        <nav className="gNav">
+          {/* Logo */}
+          <h1>
+            <Link to="/">Autoradiograph</Link>
+          </h1>
+
+          {/* Links */}
+          <ul className="navLink">
+            <li>
+              <NavLink to="/" exact>
+                Top
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/history">History</NavLink>
+            </li>
+            <li>
+              <NavLink to="/shortmovie">Short movie</NavLink>
+            </li>
+            <li>
+              <NavLink to="/">Print</NavLink>
+            </li>
+            <li>
+              <NavLink to="/">Contact</NavLink>
+            </li>
+            <li>
+              <NavLink to="/">JP / EN</NavLink>
+            </li>
+          </ul>
+
+          {/* CV */}
+          <ul className="navCV">
+            <li>
+              <NavLink to="/">Patreon</NavLink>
+            </li>
+            <li>
+              <NavLink to="/">Log in</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <nav className="spMenu">
+          <h3>Menu</h3>
+          <button
+            onClick={() => this.onSpMenuHandle()}
+            className={this.getSpMenuClasses()}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </nav>
+      </React.Fragment>
     );
   }
 }
