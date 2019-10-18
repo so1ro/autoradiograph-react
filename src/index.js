d589e193818babe051493715b278e7de81431501
MK <masamichi.kagaya@gmail.com>
2019-10-18 22:42:52 +0900

    Try core-js/raf

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import "react-app-polyfill/ie11";
+import "core-js/es/map";
+import "core-js/es/set";
+import "raf/polyfill";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
