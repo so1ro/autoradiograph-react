2407be3492be9e59047ca16941261251241db0d8
MK <masamichi.kagaya@gmail.com>
2019-11-18 07:33:27 +0900

    Add statement

--- src/components/top/statement.jsx
+++ src/components/top/statement.jsx
@@ -1,35 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { getStatement, getComments } from "./data/statement";
 import "./css/statement.styl";
 import downArrow from "./imgs/down-arrow.svg";
 
-const Statement = ({ lang }) => {
-  const statement = getStatement();
-  const comments = getComments();
+class Statement extends Component {
+  state = {
+    statementOpen: false
+  };
 
-  return (
-    <>
-      <section className="statement">
-        {statement[lang]}
-        <div className="cover"></div>
-        <img onClick={this.handleStatement()} src={downArrow} alt="" />
-      </section>
-      <section className="comments">
-        {comments.map(comment => (
-          <div key={comment._id} className="cardWrap">
-            <div className="card">
-              <div className="comment">{comment[lang]}</div>
-              <figure className={"avatar " + comment.avatarName}></figure>
-              <div className="nameWrap">
-                <p className="name">{comment.name[lang]}</p>
-                <p className="jobTitle">{comment.jobTitle[lang]}</p>
+  handleStatement = () => {
+    const currentState = this.state.statementOpen;
+    this.setState({ statementOpen: !currentState });
+  };
+
+  render() {
+    const statement = getStatement();
+    const comments = getComments();
+    const { lang } = this.props;
+
+    return (
+      <>
+        <section
+          className={`statement ${this.state.statementOpen ? "open" : ""}`}
+        >
+          <p>{statement[lang]}</p>
+          <div className="cover"></div>
+          <img onClick={this.handleStatement} src={downArrow} alt="" />
+        </section>
+        <section className="comments">
+          {comments.map(comment => (
+            <div key={comment._id} className="cardWrap">
+              <div className="card">
+                <div className="comment">{comment[lang]}</div>
+                <figure className={"avatar " + comment.avatarName}></figure>
+                <div className="nameWrap">
+                  <p className="name">{comment.name[lang]}</p>
+                  <p className="jobTitle">{comment.jobTitle[lang]}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </section>
-    </>
-  );
-};
+          ))}
+        </section>
+      </>
+    );
+  }
+}
 
 export default Statement;
