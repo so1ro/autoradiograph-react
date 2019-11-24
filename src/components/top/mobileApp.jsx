17be0101f8ec85cdac673f768e71b9cdfc993945
MK <masamichi.kagaya@gmail.com>
2019-11-24 21:05:12 +0900

    Before testing CSS module

--- src/components/top/mobileApp.jsx
+++ src/components/top/mobileApp.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import "./css/mobileApp.styl";
+import bnrAppStore from "./imgs/bnr-app-store.svg";
+import bnrGooglePlay from "./imgs/bnr-google-play.svg";
 
 const MobileApp = ({ lang }) => {
   return (
-    <div className="mobileApp">
-      <h1>MobileApp</h1>
-    </div>
+    <section className="mobileApp">
+      <div className="wrap">
+        <div className="text">
+          <h1>App</h1>
+          <p>
+            You can see all of archives
+            <br />
+            through Autoradiograph App
+            <br />
+            <br />
+            $20
+          </p>
+        </div>
+        <div className="movie">
+          <div className="tabletWrap">
+            <div className="tablet">
+              <video src="" width="372" height="546"></video>
+            </div>
+          </div>
+          <div className="spWrap">
+            <div className="sp">
+              <video src="" width="400" height="572"></video>
+            </div>
+          </div>
+        </div>
+        <figure className="banner">
+          <img src={bnrAppStore} alt="Apple Store" />
+          <img src={bnrGooglePlay} alt="Google Play" />
+        </figure>
+      </div>
+    </section>
   );
 };
 
