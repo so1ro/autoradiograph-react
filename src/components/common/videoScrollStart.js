8c0ac27206810a84752b8a4340fd66f544669175
MK <masamichi.kagaya@gmail.com>
2019-12-06 15:18:28 +0900

    Before changing App video with lang/sp/tablet

---
+++ src/components/common/videoScrollStart.js
@@ -0,0 +1,13 @@
+// export function videoScrollStart(selector) {
+
+//     console.log(selector);
+//     // selector.scrollTop
+// }
+
+// videoScrollStart(videoWrap);
+console.log("working!!!!!!!!");
+
+document.window.addEventListener("scroll", () => {
+  let scrollY = window.scrollY;
+  console.log("scrollY:", scrollY);
+});
--- src/components/top/mobileApp.jsx
+++ src/components/top/mobileApp.jsx
@@ -1,42 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import "./css/mobileApp.styl";
 import bnrAppStore from "./imgs/bnr-app-store.svg";
 import bnrGooglePlay from "./imgs/bnr-google-play.svg";
+import tabVideo from "./videos/video_tb_jp.mp4";
+import smtVideo from "./videos/video_sp_jp.mp4";
+// import {videoScrollStart} from '../common/videoScrollStart';
 
-const MobileApp = ({ lang }) => {
-  return (
-    <section className="mobileApp">
-      <div className="wrap">
-        <div className="text">
-          <h1>App</h1>
-          <p>
-            You can see all of archives
-            <br />
-            through Autoradiograph App
-            <br />
-            <br />
-            $20
-          </p>
-        </div>
-        <div className="movie">
-          <div className="tabletWrap">
-            <div className="tablet">
-              <video src="" width="372" height="546"></video>
-            </div>
+class MobileApp extends Component {
+  state = {
+    videoPlay: false
+  };
+  // movie = React.createRef();
+
+  render() {
+    // const { videoPlay } = this.state;
+
+    // // const movie = document.querySelector(".movie");
+    // console.log("movie:", this.movie.current);
+
+    // document.addEventListener("scroll", () => {
+    //   let scrollY = window.scrollY;
+    //   let videoPositionY = 112;
+    //   console.log("scrollY:", scrollY);
+    // });
+
+    return (
+      <section className="mobileApp">
+        <div className="wrap">
+          <div className="text">
+            <h1>App</h1>
+            <p>
+              You can see all of archives
+              <br />
+              through Autoradiograph App
+              <br />
+              <br />
+              $20
+            </p>
           </div>
-          <div className="spWrap">
-            <div className="sp">
-              <video src="" width="400" height="572"></video>
-            </div>
+          <div ref={this.movie} className="movie">
+            <video
+              className="tabletWrap"
+              loop
+              autoPlay
+              muted
+              src={tabVideo}
+              // loop={videoPlay}
+              // autoPlay={videoPlay}
+              // muted={videoPlay}
+            />
+            <video className="spWrap" src={smtVideo} loop autoPlay muted />
           </div>
+          <figure className="banner">
+            <img src={bnrAppStore} alt="Apple Store" />
+            <img src={bnrGooglePlay} alt="Google Play" />
+          </figure>
         </div>
-        <figure className="banner">
-          <img src={bnrAppStore} alt="Apple Store" />
-          <img src={bnrGooglePlay} alt="Google Play" />
-        </figure>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 export default MobileApp;
