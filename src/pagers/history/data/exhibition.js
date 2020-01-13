cc9e4c93c0284cd99657c18ccee9000547d4551f
MK <masamichi.kagaya@gmail.com>
2020-01-13 12:02:18 +0900

    Modify exhibition / ／

--- src/pagers/history/data/exhibition.js
+++ src/pagers/history/data/exhibition.js
@@ -3,24 +3,24 @@ const exhibitionList = [
     _id: 29,
     date: { en: "Sep. 2019", jp: "2019 / 9" },
     place: {
-      en: "Korea Seoul / MMCA - Gathering",
-      jp: "韓国 / 韓国国立現代美術館主催「Gathering」"
+      en: "Korea Seoul ／ MMCA - Gathering",
+      jp: "韓国 ／ 韓国国立現代美術館主催「Gathering」"
     }
   },
   {
     _id: 28,
     date: { en: "Sep. 2019", jp: "2019 / 9" },
     place: {
-      en: "Spain / Getxophoto Festival",
-      jp: "スペイン / Getxophoto Festival"
+      en: "Spain ／ Getxophoto Festival",
+      jp: "スペイン ／ Getxophoto Festival"
     }
   },
   {
     _id: 27,
     date: { en: "Sep. 2018", jp: "2018 / 9" },
     place: {
-      en: "Netherlands / BredaPhoto Festival",
-      jp: "オランダ / BredaPhoto Festival"
+      en: "Netherlands ／ BredaPhoto Festival",
+      jp: "オランダ ／ BredaPhoto Festival"
     }
   },
   {
@@ -28,7 +28,7 @@ const exhibitionList = [
     date: { en: "Apr. 2018", jp: "2018 / 4" },
     place: {
       en: "Ukraine Zhytomyr Polytechnic State University",
-      jp: "ウクライナ / ジトミール工科大学"
+      jp: "ウクライナ ／ ジトミール工科大学"
     }
   },
   {
@@ -43,32 +43,32 @@ const exhibitionList = [
     _id: 24,
     date: { en: "Dec. 2017", jp: "2017 / 12" },
     place: {
-      en: "China / Lianzhou Foto Festival",
-      jp: "中国 / 連州国際写真祭"
+      en: "China ／ Lianzhou Foto Festival",
+      jp: "中国 ／ 連州国際写真祭"
     }
   },
   {
     _id: 23,
     date: { en: "Sep. 2017", jp: "2017 / 9" },
     place: {
-      en: "Austria / Ars Electronica",
-      jp: "オーストリア / Ars Electronica"
+      en: "Austria ／ Ars Electronica",
+      jp: "オーストリア ／ Ars Electronica"
     }
   },
   {
     _id: 22,
     date: { en: "Mar. 2017", jp: "2017 / 3" },
     place: {
-      en: "England / FORMAT Photography Festival",
-      jp: "England / FORMAT Photography Festival"
+      en: "England ／ FORMAT Photography Festival",
+      jp: "England ／ FORMAT Photography Festival"
     }
   },
   {
     _id: 21,
     date: { en: "Mar. 2017", jp: "2017 / 3" },
     place: {
-      en: "Tokyo / Symposium in Hosei university",
-      jp: "法政大学 / 日本野鳥の会主催 シンポジウム"
+      en: "Tokyo ／ Symposium in Hosei university",
+      jp: "法政大学 ／ 日本野鳥の会主催 シンポジウム"
     }
   },
   {
@@ -76,7 +76,7 @@ const exhibitionList = [
     date: { en: "Oct. 2017", jp: "2017 / 10" },
     place: {
       en: "Higashimatsuyama city in Saitama",
-      jp: "埼玉県東松山市 / 「みる・知る・くらす展」"
+      jp: "埼玉県東松山市 ／ 「みる・知る・くらす展」"
     }
   },
   {
@@ -108,7 +108,7 @@ const exhibitionList = [
     date: { en: "June 2016", jp: "2016 / 6" },
     place: {
       en: "Warabi city, Suginami ward, Ichikawa city",
-      jp: "埼玉県蕨市、東京都杉並区、千葉県市川市 / 首都圏リレー展示"
+      jp: "埼玉県蕨市、東京都杉並区、千葉県市川市 ／ 首都圏リレー展示"
     }
   },
   {
@@ -116,7 +116,7 @@ const exhibitionList = [
     date: { en: "May 2016", jp: "2016 / 5" },
     place: {
       en: "Shiki city, Yokohama city",
-      jp: "埼玉県志木市、神奈川県横浜市 / 首都圏リレー展示"
+      jp: "埼玉県志木市、神奈川県横浜市 ／ 首都圏リレー展示"
     }
   },
   {
@@ -155,16 +155,16 @@ const exhibitionList = [
     _id: 10,
     date: { en: "Apr. 2015", jp: "2015 / 4" },
     place: {
-      en: "Canada Québec / World Uranium Symposium",
-      jp: "カナダ ケベック / World Uranium Symposium"
+      en: "Canada Québec ／ World Uranium Symposium",
+      jp: "カナダ ケベック ／ World Uranium Symposium"
     }
   },
   {
     _id: 9,
     date: { en: "Mar. 2015", jp: "2015 / 3" },
     place: {
-      en: "Paris / Galarie planète rouge",
-      jp: "パリ / Galarie planète rouge "
+      en: "Paris ／ Galarie planète rouge",
+      jp: "パリ ／ Galarie planète rouge "
     }
   },
   {
