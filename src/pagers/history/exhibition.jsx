3bdd0cf4aadfa0571a3526945e23e963f5069232
MK <masamichi.kagaya@gmail.com>
2020-01-13 14:45:43 +0900

    Add PictureGrid to exhibition.jsx

--- src/pagers/history/exhibition.jsx
+++ src/pagers/history/exhibition.jsx
@@ -3,7 +3,8 @@ import { getExhibitionList } from "./data/exhibition";
 import "./css/exhibition.styl";
 import { getActive } from "../../components/common/getActiveClass";
 import { getJpClass } from "../../components/common/getJpClass";
-// import PictureGrid from "../../components/pictureGrid";
+import PictureGrid from "../../components/pictureGrid";
+import { pressList as exhibitPictureList } from "./data/press";
 
 class Exhibition extends Component {
   render() {
@@ -12,7 +13,7 @@ class Exhibition extends Component {
 
     return (
       <div className={"exhibition" + getActive(tabSelect, "exhibition")}>
-        <ul>
+        <ul className="table">
           {exhibitionList.map(exhibition => (
             <React.Fragment key={exhibition._id}>
               <li className="date">{exhibition.date[lang]}</li>
@@ -22,7 +23,7 @@ class Exhibition extends Component {
             </React.Fragment>
           ))}
         </ul>
-        {/* <PictureGrid lang={lang} items={exhibitPictureList} /> */}
+        <PictureGrid lang={lang} items={exhibitPictureList} />
       </div>
     );
   }
